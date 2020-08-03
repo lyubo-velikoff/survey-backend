d3f8dfeebd6dd69efa9035fd8595e3f2570326bd
Lyubo Velikov <lyubo.velikov@chameleontechnology.co.uk>
2020-08-03 14:12:17 +0100

    feat: Add erd generator.

---
+++ tasks/generate-erd.js
@@ -0,0 +1,8 @@
+const { sequelize } = require('../models/index.js')
+
+// Require our models. Running each module registers the model into sequelize
+// so any other part of the application can simply call sequelize.model('User')
+// to get access to the User model.
+require('../models')
+
+module.exports = sequelize
